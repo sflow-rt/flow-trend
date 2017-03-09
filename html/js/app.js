b90d78b652fda517ae5573503814b6413f490408
pphaal <peter.phaal@gmail.com>
2017-03-08 21:12:36 -0800

    Initial commit

---
+++ html/js/app.js
@@ -0,0 +1,461 @@
+$(function() { 
+  var restPath =  '../scripts/top.js/';
+  var shortcutsURL = restPath + 'shortcuts/json';
+  var keysURL =  restPath + 'flowkeys/json';
+  var topURL = restPath + 'flows/json';
+
+  var SEP = '_SEP_';
+
+  var db = {};
+
+  var defaults = {
+    tab:0,
+    hlp0:'show',
+    hlp1:'hide',
+    hlp2:'hide',
+    keys:'',
+    value:'',
+    filter:'',
+    topshow:50,
+  };
+
+  var state = {};
+  $.extend(state,defaults);
+		
+  function nf(value,fix) {
+    var suffixes = ["\u00B5", "m", "", "K", "M", "G", "T", "P", "E"];
+    if (value === 0) return value;
+    var i = 2;
+    var divisor = 1;
+    var factor = 1000;
+    var absVal, scaled;
+    absVal = Math.abs(value);
+    while (i < suffixes.length) {
+      if ((absVal / divisor) < factor) {
+        break;
+      }
+      divisor *= factor;
+      i++;
+    }
+    scaled = Math.round(absVal * factor / divisor) / factor;
+    if(fix) scaled = scaled.toFixed(fix);
+    return scaled + suffixes[i];
+  };
+
+  function createQuery(params) {
+    var query, key, value;
+    for(key in params) {
+      value = params[key];
+      if(value === defaults[key]) continue;
+      if(query) query += '&';
+      else query = '';
+      query += encodeURIComponent(key)+'='+encodeURIComponent(value);
+    }
+    return query;
+  }
+
+  function getState(key, defVal) {
+    return window.sessionStorage.getItem(key) || state[key] || defVal;
+  }
+
+  function setState(key, val, showQuery) {
+    state[key] = val;
+    window.sessionStorage.setItem(key, val);
+    if(showQuery) {
+      var query = createQuery(state);
+      window.history.replaceState({},'',query ? '?' + query : './');
+    }
+  }
+
+  function setQueryParams(query) {
+    var vars = query.split('&');
+    var params = {};
+    for(var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if(pair.length === 2) setState(decodeURIComponent(pair[0]), decodeURIComponent(pair[1]),false);
+    }
+  }
+
+  var search = window.location.search;
+  if(search) setQueryParams(search.substring(1));
+
+  $('#help-acc > div').each(function(idx) {
+    $(this).accordion({
+      heightStyle:'content',
+      collapsible: true,
+      active: getState('hlp'+idx, 'hide') === 'show' ? 0 : false,
+      activate: function(event, ui) {
+        var newIndex = $(this).accordion('option','active');
+        setState('hlp'+idx, newIndex === 0 ? 'show' : 'hide', true);
+      }
+    });
+  });
+        
+  $('#tabs').tabs({
+    active: getState('tab', 0),
+    activate: function(event, ui) {
+      var newIndex = ui.newTab.index();
+      setState('tab', newIndex, true);
+      $.event.trigger({type:'updateChart'});
+    },
+    create: function(event,ui) {
+      $.event.trigger({type:'updateChart'});
+    }
+  });
+
+  $('#clone_button').button({icons:{primary:'ui-icon-newwin'},text:false}).click(function() {
+     window.open(window.location);
+  }); 
+
+  var top_keys = getState('keys','');
+  var top_value = getState('value','');
+  var top_filter = getState('filter','');
+	
+  $('#keys')
+    .val(top_keys)
+    .bind( "keydown", function( event ) {
+      if ( event.keyCode === $.ui.keyCode.TAB &&
+        $( this ).autocomplete( "instance" ).menu.active ) {
+	   event.preventDefault();
+        }
+      })
+    .autocomplete({
+      minLength: 0,
+      source: function( request, response) {
+	$.getJSON(keysURL, { search: request.term.split(/,\s*/).pop() }, response)
+      },
+      focus: function() {
+        // prevent value inserted on focus
+        return false;
+      },
+      select: function( event, ui ) {
+        var terms = this.value.split(/,\s*/);
+        // remove the current input
+        terms.pop();
+        // add the selected item
+        terms.push( ui.item.value );
+        // add placeholder to get the comma-and-space at the end
+        terms.push( "" );
+        this.value = terms.join( "," );
+        return false;
+      }
+    })
+    .focus(function() { $(this).autocomplete('search'); });
+
+  $('#value')
+    .val(top_value)
+    .autocomplete({
+       minLength:0,
+       source:['bps', 'Bps', 'fps']
+    })
+    .focus(function() { $(this).autocomplete('search'); });
+	
+  $('#filter')
+    .val(top_filter)
+    .bind( "keydown", function( event ) {
+      if ( event.keyCode === $.ui.keyCode.TAB &&
+        $( this ).autocomplete( "instance" ).menu.active ) {
+          event.preventDefault();
+        }
+    })
+    .autocomplete({
+      minLength: 0,
+      source: function( request, response) {
+        $.getJSON(keysURL, { search: request.term.split(/[&|(]\s*/).pop() }, response)
+      },
+      focus: function() {
+        // prevent value inserted on focus
+        return false;
+      },
+      select: function( event, ui ) {
+        var terms = this.value.split(/,\s*/);
+        // remove the current input
+        terms.pop();
+        // add the selected item
+        terms.push( ui.item.value );
+        // add placeholder to get the comma-and-space at the end
+        terms.push( "" );
+        this.value = terms.join( "=" );
+        return false;
+      }
+    })
+    .focus(function() { $(this).autocomplete('search'); });
+
+  $('#cleardef').button({icons:{primary:'ui-icon-cancel'},text:false}).click(function() {
+    $('#keys').val('');
+    $('#value').val('');
+    $('#filter').val('');
+    top_keys = '';
+    top_value = '';
+    top_filter = '';
+    setState('keys',top_keys);
+    setState('value',top_value);
+    setState('filter',top_filter,true);
+    emptyTopFlows();
+  });
+  $('#submitdef').button({icons:{primary:'ui-icon-check'},text:false}).click(function() {
+    top_keys = $.trim($('#keys').val()).replace(/(,$)/g, "");
+    top_value = $.trim($('#value').val());
+    top_filter = $.trim($('#filter').val());
+    setState('keys',top_keys);
+    setState('value',top_value);
+    setState('filter',top_filter,true);
+    emptyTopFlows();   
+  });
+  function valueToKey(val) {
+    var key;
+    switch(val) {
+    case 'bps': 
+      key = 'bytes'; 
+      break;
+    case 'Bps': 
+      key = 'bytes'; 
+      break;
+    case 'fps': 
+      key = 'frames'; 
+      break;
+    default: 
+      key = val;
+    }
+    return key;
+  }
+
+  function valueToScale(val) {
+    return 'bps' === val ? 8 : 1;
+  }
+
+  function valueToTitle(val) {
+    var title;
+    switch(val) {
+    case 'bps': 
+      title = 'Bits per Second'; 
+      break;
+    case 'bytes':
+      case 'Bps': 
+      title = 'Bytes per Second'; 
+      break;
+    case 'frames':
+      case 'fps': 
+      title  = 'Frames per Second'; 
+      break;
+    case 'requests':
+      title = 'Requests per Second';
+      break;
+    default: 
+      title = val;
+    }
+    return title;
+  }
+
+  function addFilter(key, value, filter) {
+    var newFilter = filter;
+    if(!newFilter) newFilter = "";
+    if(newFilter.length > 0) newFilter += "&";
+    newFilter += key + "='" + value + "'";
+    $('#filter').val(newFilter);	 
+    top_filter = newFilter;
+    setState('filter', top_filter, true);
+    emptyTopFlows();
+  }
+
+  function updateTopFlows(data,query,scale,title) {
+    if($('#shortcutstable_wrapper').is(':visible')) return;
+
+  }
+
+  var $shortcutsTable;
+  function initializeShortcutsTable() {
+    $shortcutsTable = $('#shortcutstable').DataTable({
+      ajax: {
+        url: shortcutsURL,
+        dataSrc: function(data) { 
+          return data; 
+        }
+      },
+      deferRenderer: true,
+      columns:[
+        {data:'category'},
+        {data:'protocol'},
+        {data:'description'}
+      ],
+      columnDefs: [ { targets: 2, orderable: false } ]
+    })
+    .page.len(getState('topshow'))
+    .on('length', function(e,settings,len) {
+      setState('topshow', len, true);
+    })
+    .on('xhr', function(e,settings,json) {
+      var len = json.length || 0;
+      $('#numshortcuts').val(len).removeClass(len ? 'error' : 'good').addClass(len ? 'good' : 'error');;
+    })
+    .on('click', 'tr', function(e) {
+      var row = $shortcutsTable.row($(this));
+      var shortcut = row.data();
+      if(!shortcut) return;		
+      top_keys = shortcut.keys || '';
+      top_value = shortcut.value || '';
+      top_filter = shortcut.filter || '';
+      $('#keys').val(top_keys);
+      $('#value').val(top_value);
+      $('#filter').val(top_filter);
+      setState('keys', top_keys, false);
+      setState('value', top_value, false);
+      setState('filter', top_filter, true);
+      emptyTopFlows();
+    });
+  }
+
+  function updateData(data) {
+    if(!data 
+      || !data.trend 
+      || !data.trend.times 
+      || data.trend.times.length == 0) return;
+
+    if(db.trend) {
+      // merge in new data
+      var maxPoints = db.trend.maxPoints;
+      var remove = db.trend.times.length > maxPoints ? db.trend.times.length - maxPoints : 0;
+      db.trend.times = db.trend.times.concat(data.trend.times);
+      if(remove) db.trend.times = db.trend.times.slice(remove);
+      for(var name in db.trend.trends) {
+        db.trend.trends[name] = db.trend.trends[name].concat(data.trend.trends[name]);
+        if(remove) db.trend.trends[name] = db.trend.trends[name].slice(remove);
+      }
+    } else db.trend = data.trend;
+
+    db.trend.start = new Date(db.trend.times[0]);
+    db.trend.end = new Date(db.trend.times[db.trend.times.length - 1]);
+
+    $.event.trigger({type:'updateChart'});
+  }
+
+
+  var running_topflows;
+  var timeout_topflows;
+  function pollTopFlows() {
+    running_topflows = true;
+    var query = {keys:top_keys,value:valueToKey(top_value),filter:top_filter};
+    if(db.trend && db.trend.end) query.after=db.trend.end.getTime();
+    var scale = valueToScale(top_value);
+    var title = valueToTitle(top_value);
+    $.ajax({
+      url: topURL,
+      data: query,
+      success: function(data) {
+        if(running_topflows) {
+          updateData(data);
+          timeout_topflows = setTimeout(pollTopFlows, 1000);
+        }
+      },
+      error: function(result,status,errorThrown) {
+        if(running_topflows) timeout_topflows = setTimeout(pollTopFlows, 5000);
+      }
+    });
+  }
+
+  function stopPollTopFlows() {
+    running_topflows = false;
+    if(timeout_topflows) clearTimeout(timeout_topflows);
+  }
+
+  function emptyTopFlows() {
+    stopPollTopFlows();
+    if(db.trend) {
+      $(document).off('updateChart');
+      $('#topn').stripchart('destroy');
+      $('#topn').empty();
+      delete db.trend;
+    }
+    if(!top_keys || !top_value) {
+      $('#shortcutstable_wrapper').show();
+      $('#topn').hide();
+      return;
+    }
+    $('#shortcutstable_wrapper').hide();
+    $('#topn').show();
+
+    if(!db.trend) {
+       $('#topn').chart({
+          type: 'topn',
+          legendHeadings: top_keys.split(','),
+          units:valueToTitle(top_value),
+          stack: true,
+          sep: SEP,
+          metric: 'topn'
+       },db);
+    }
+
+    var query = {keys:top_keys,value:valueToKey(top_value),filter:top_filter};
+    var scale = valueToScale(top_value);
+    var title = valueToTitle(top_value);
+    updateTopFlows([],query,scale,title);
+    pollTopFlows();
+  }
+
+  $('#topn').click(function(e) {
+    var idx,key,val,tgt = $(e.target);
+    if(tgt.is('td')) {
+      idx = tgt.index() - 1;
+      key = top_keys.split(',')[idx];
+      val = tgt.text();
+      addFilter(key,val,top_filter);
+    }
+    else if(tgt.is('div') && tgt.parent().is('td')) {
+      var row = tgt.parent().parent();
+      row.children().each(function(i,td) {
+        if(i>0) {
+          idx = i - 1;
+          key = top_keys.split(',')[idx];
+          val = $(td).text();
+          addFilter(key,val,top_filter);
+        }
+      });
+    }
+  });
+
+  function refreshShortcuts() {
+    $shortcutsTable.ajax.reload();
+  } 
+
+  function getShortcuts() {
+    $.ajax({
+      url:shortcutsURL,
+      dataType: 'json',
+      success: function(data) {
+        location.href = 'data:application/json,' + encodeURIComponent(JSON.stringify(data,null,1));
+      }
+    });
+  }
+      
+  function warningDialog(message) {
+    $('<div>' + message + '</div>').dialog({dialogClass:'alert', modal:true, buttons:{'Close': function() { $(this).dialog('close'); }}})
+  }
+
+  $('#shortcutsrefresh').button({icons:{primary:'ui-icon-arrowrefresh-1-e'},text:false}).click(refreshShortcuts);
+  $('#shortcutsget').button({icons:{primary:'ui-icon-search'},text:false}).click(getShortcuts);
+  $('#shortcutsfile').hide().change(function(evt) {
+    var input = event.target;
+    var reader = new FileReader();
+    $this = $(this);
+    reader.onload = function(){
+      var text = reader.result;
+      $this.wrap('<form>').closest('form').get(0).reset();
+      $this.unwrap();
+      $.ajax({
+        url:shortcutsURL,
+        type: 'POST',
+        contentType:'application/json',
+        data:text,
+        success:refreshShortcuts,
+        error: function() { warningDialog('Badly formatted shortcuts'); }
+      });
+    };
+    reader.readAsText(input.files[0]);
+  });
+  $('#shortcutsset').button({icons:{primary:'ui-icon-arrowstop-1-n'},text:false}).click(function() {$('#shortcutsfile').click();});
+
+  $(document).ready(function() {
+    initializeShortcutsTable();
+    emptyTopFlows();
+  });
+});
---
+++ scripts/top.js
@@ -0,0 +1,158 @@
+// author: InMon Corp.
+// version: 1.0
+// date: 3/2/2017
+// description: Flow Trend
+// copyright: Copyright (c) 2017 InMon Corp. ALL RIGHTS RESERVED
+
+include(scriptdir()+'/inc/trend.js');
+
+var SEP = '_SEP_';
+
+var defaultShortcuts = [
+{category:'Traffic', protocol:'IP', description:'Sources',keys:'ipsource',value:'bps',filter:''},
+{category:'Traffic', protocol:'IP', description:'Destinations',keys:'ipdestination',value:'bps',filter:''},
+{category:'Traffic', protocol:'IP', description:'Source-Destination pairs',keys:'ipsource,ipdestination',value:'bps',filter:''},
+{category:'Traffic', protocol:'IPv6', description:'Sources',keys:'ip6source',value:'bps',filter:''},
+{category:'Traffic', protocol:'IPv6', description:'Destinations',keys:'ip6destination',value:'bps',filter:''},
+{category:'Traffic', protocol:'IPv6', description:'Source-Destination pairs',keys:'ip6source,ip6destination',value:'bps',filter:''},
+{category:'Traffic', protocol:'Ethernet', description:'Sources',keys:'macsource,oui:macsource:name',value:'frames',filter:''},
+{category:'Traffic', protocol:'Ethernet', description:'Sources of broadcasts',keys:'macsource,oui:macsource:name',value:'frames',filter:'macdestination=FFFFFFFFFFFF'},
+{category:'Security', protocol:'TCP', description:'Connection attempts',keys:'ipsource,ipdestination,tcpdestinationport',value:'frames',filter:'tcpflags~.......1.'},
+{category:'Security', protocol:'ARP', description:'Source of requests',keys:'arpipsender',value:'requests',filter:'arpoperation=1'},
+{category:'Security', protocol:'DNS', description:'Requested domains',keys:'dnsqname',value:'requests',filter:''},
+{category:'Security', protocol:'DNS', description:'Clients',keys:'or:ipsource:ip6source',value:'requests',filter:'dnsqr=false'},
+{category:'Security', protocol:'DNS', description:'Servers',keys:'or:ipsource:ip6source',value:'requests',filter:'dnsqr=true'},
+{category:'Security', protocol:'ICMP', description:'Unreachable ports', keys:'or:ipdestination:ip6destination,icmpunreachableport', value:'fps', filter:''},
+{category:'Security', protocol:'ICMP', description:'Unreachable protocols', keys:'or:ipdestination:ip6destination,icmpunreachableprotocol', value:'fps', filter:''},
+{category:'Security', protocol:'ICMP', description:'Unreachable hosts', keys:'or:ipdestination:ip6destination,icmpunreachablehost', value:'fps', filter:''},
+{category:'Security', protocol:'ICMP', description:'Unreachable networks', keys:'or:ipdestination:ip6destination,icmpunreachablenet', value:'fps', filter:''},
+{category:'Virtualization', protocol:'VxLAN', description:'Tenant VNI', keys:'vni',value:'bps',filter:''},
+{category:'Virtualization', protocol:'NVGRE', description:'Tenant VSID', keys:'grevsid', value:'bps',filter:''},
+{category:'Virtualization', protocol:'Geneve', description:'Tenant VNI', keys:'genevevni',value:'bps',filter:''}
+];
+
+var aggMode  = getSystemProperty('flow-trend.aggMode')  || 'max';
+var maxFlows = getSystemProperty('flow-trend.maxFlows') || 10;
+var minValue = getSystemProperty('flow-trend.minValue') || 0.01;
+var agents   = getSystemProperty('flow-trend.agents')   || 'ALL';
+ 
+var shortcuts = storeGet('shortcuts') || defaultShortcuts;
+
+var userFlows = {};
+
+function escapeRegExp(str) {
+  // seems like a bug - Rhino doesn't convert Java strings into native JavaScript strings
+  str = new String(str);
+  return str ? str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") : null;
+}
+
+var specID = 0;
+function flowSpec(keys,value,filter) {
+  var keysStr = keys ? keys.join(',') : '';
+  var valueStr = value ? value.join(',') : '';
+  var filterStr = filter ? filter.join('&') : '';
+
+  if(keysStr.length === 0 || valueStr.length === 0) return null;
+
+  var key = keysStr || '';
+  if(valueStr) key += '#' + valueStr;
+  if(filterStr) key += '#' + filterStr;
+  var entry = userFlows[key];
+  if(!entry) {
+    // try to create flow
+    var name = 'flow_trend_' + specID;
+    try {
+      setFlow(name,{keys:keysStr, value:valueStr, filter: filterStr.length > 0 ? filterStr : null, t:2, n:maxFlows, fs:SEP});
+      entry = {name:name, trend: new Trend(300,1)};
+      entry.trend.addPoints({topn:{}});
+      userFlows[key] = entry;
+      specID++;
+    } catch(e) {
+      entry = null;
+    }
+  }
+  if(!entry) return null;
+  entry.lastQuery = (new Date()).getTime();
+
+  return entry;
+}
+
+setIntervalHandler(function() {
+  var key, entry, top, topN, i, now = (new Date()).getTime();
+  for(key in userFlows) {
+    entry = userFlows[key];
+    if(now - entry.lastQuery > 10000) {
+      clearFlow(entry.name);
+      delete userFlows[key];
+    } else {
+      topN = {};
+      top = activeFlows(agents,entry.name,maxFlows,minValue,aggMode);
+      if(top) {
+        for(i = 0; i < top.length; i++) {
+          topN[top[i].key] = top[i].value;
+        }
+      }
+      entry.trend.addPoints({topn:topN}); 
+    }
+  }
+},1);
+
+function validShortcuts(obj) {
+  if(!Array.isArray(obj)) return false;
+  var attrs = ['category','protocol','description','keys','value','filter'];
+  for(var i = 0; i < obj.length; i++) {
+    let shortcut = obj[i];
+    for(var j = 0; j < attrs.length; j++) {
+      let attr = attrs[j];
+      if(!shortcut.hasOwnProperty(attr)) return false;
+      if(typeof shortcut[attr] !== 'string') return false;
+    }
+  }
+  return true;
+}
+
+setHttpHandler(function(req) {
+  var result, trend, key, entry, path = req.path;
+  if(!path || path.length === 0) throw "not_found";
+     
+  switch(path[0]) {
+  case 'shortcuts':
+    if(path.length > 1) throw "not_found";
+    switch(req.method) {
+    case 'POST':
+    case 'PUT':
+      if(req.error) throw "bad_request";
+      if(!validShortcuts(req.body)) throw "bad_request";
+        shortcuts = req.body;
+	storeSet('shortcuts', shortcuts);
+	break;
+    default: return shortcuts;
+    }
+    break;
+  case 'flowkeys':
+    if(path.length > 1) throw "not_found";
+    result = [];
+    var search = req.query['search'];
+    if(search) {
+      var matcher = new RegExp('^' + escapeRegExp(search), 'i');
+      for(key in flowKeys()) {
+        if(matcher.test(key)) result.push(key);
+      }
+    } else {
+      for(key in flowKeys()) result.push(key);
+    }
+    result.sort();
+    break;
+  case 'flows':
+    if(path.length > 1) throw "not_found";
+    entry = flowSpec(req.query['keys'],req.query['value'],req.query['filter']);
+    if(!entry) throw 'bad_request';
+    trend = entry.trend;
+    result = {};
+    result.trend = req.query.after ? trend.after(parseInt(req.query.after)) : trend;
+    break;
+  default: throw 'not_found';
+  } 
+  return result;
+});
+
