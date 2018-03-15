e8b053d94c3eb9eccc7cd416958ea8ef5d1463d2
pphaal <peter.phaal@gmail.com>
2018-03-15 07:01:56 -0700

    Unintended global

--- html/js/app.js
+++ html/js/app.js
@@ -408,7 +408,7 @@ $(function() {
   $('#shortcutsfile').hide().change(function(event) {
     var input = event.target;
     var reader = new FileReader();
-    $this = $(this);
+    var $this = $(this);
     reader.onload = function(){
       var text = reader.result;
       $this.wrap('<form>').closest('form').get(0).reset();
